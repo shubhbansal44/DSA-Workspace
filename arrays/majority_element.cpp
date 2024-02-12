039b13b214e784e0eaf4a9d38828ad1319ac699c
Shubhbansal44 <shubh.bansal1504@gmail.com>
2024-02-12 15:42:49 +0530

    majority code fix

--- arrays/majority_element.cpp
+++ arrays/majority_element.cpp
@@ -1,23 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int majority(const vector<int> &vect,int* max,int* fr)
+void majority(const vector<int> &vect,int* max,int* fr)
 {
-    int u=0,k=1,f=1;
+    int u=0,k=1;
     for(int i=u+1; i<vect.size(); i++)
     {
         if(vect[u]==vect[i])
         {k++;}
         if(i==vect.size()-1)
         {
-            if(k==*fr)
-            {f++;}
             if(k>*fr)
             {*fr=k;*max=vect[u];}
             u++;k=1;i=u;
         }
     }
-    return f;
+    return;
 }
 int main()
 {
@@ -31,9 +29,7 @@ int main()
         cin>> p;
     }
     int max=vect[0],fr=1;
-    int f=majority(vect,&max,&fr);
-    if(f==1)
-    {cout<< max<< " appeared "<< fr<< " times";}
-    else{cout<< "Every element has same frequency!!";}
+    majority(vect,&max,&fr);
+    cout<< max<< " appeared "<< fr<< " times";
     return 0;
 }
