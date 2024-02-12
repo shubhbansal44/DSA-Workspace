453175aa50bca427f0a8b231d5734384858be498
Shubhbansal44 <shubh.bansal1504@gmail.com>
2024-02-12 22:26:17 +0530

    added sorted insert in a linked list

---
+++ linked_list/sorted_insert_in_linked_list.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class linked_list
+{
+    public:
+    int data;
+    linked_list *next;
+    linked_list(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+linked_list *insert(linked_list *head,int value)
+{
+    linked_list *ptr=new linked_list(value);
+    if(head->data>value)
+    {
+        ptr->next=head;return ptr;
+    }
+    linked_list *nhead=head;
+    while(nhead->next!=NULL)
+    {
+        if(nhead->data<=value&&nhead->next->data>value)
+        {
+            break;
+        }
+        nhead=nhead->next;
+    }
+    if(nhead->next==NULL)
+    {nhead->next=ptr;return head;}
+    ptr->next=nhead->next;
+    nhead->next=ptr;
+    return head;
+
+}
+
+void prtls(linked_list *ptr)
+{
+    while(ptr!=NULL)
+    {
+        cout<< ptr->data<< " ";
+        ptr=ptr->next;
+    }
+}
+
+int main()
+{
+    linked_list *head=new linked_list(10);
+    head->next=new linked_list(30);
+    head->next->next=new linked_list(40);
+    head->next->next->next=new linked_list(50);
+    int value;
+    cout<< "Enter value to be inserted: ";
+    cin>> value;
+    head=insert(head,value);
+    prtls(head);
+    return 0;
+}
